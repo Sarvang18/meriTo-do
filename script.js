53403ebaaab4a2439062e648a7d83d46cd974524
sarvangpro@gmail.com <sarvangchaturvedi1@gmail.com>
2024-07-24 03:39:24 +0530

    first

---
+++ script.js
@@ -0,0 +1,36 @@
+const inputBox=document.querySelector('.inputLine>input')
+const addTaskBTN=document.querySelector('.addBTN')
+const taskHolder=document.querySelector('.taskHolder')
+
+addTaskBTN.addEventListener('click',()=>{
+    const task=document.createElement('li')
+    const taskText=inputBox.value
+    task.innerText=taskText
+    task.classList.add('btmMargin')
+    console.log(task)
+
+    const deleteTask=document.createElement('button')
+    deleteTask.innerText='x'
+    deleteTask.classList.add('deleteTask')
+
+    if(taskText){
+        taskHolder.appendChild(task)
+        task.appendChild(deleteTask)
+        
+    }
+    //append nhi appendChild karte hain
+
+
+    deleteTask.addEventListener('click',(evt)=>{
+        task.style.display='none';
+    })
+    
+
+    task.addEventListener('click',(evt)=>{
+        console.log(taskText)
+        task.style.textDecoration='line-through'
+        task.style.opacity=0.3
+        deleteTask.innerText='✔'
+    })
+})
+
